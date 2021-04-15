db9f51544ca64a01980017a0e8ffadbe57f92872
jdavidtorres <jesu_ti91@hotmail.com>
2021-04-15 11:11:08 -0500

    Notación Transactional que ayudan al manejo de transacciones

--- src/main/java/com/jdti/shoppingcart/services/ProductServiceImpl.java
+++ src/main/java/com/jdti/shoppingcart/services/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import com.jdti.shoppingcart.models.entities.ProductEntity;
 import com.jdti.shoppingcart.models.repositories.IProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,21 +15,25 @@ public class ProductServiceImpl implements IProductService {
     @Autowired
     private IProductRepository iProductRepository;
 
+    @Transactional
     @Override
     public ProductEntity save(ProductEntity product) {
         return iProductRepository.save(product);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<ProductEntity> findAll() {
         return iProductRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Optional<ProductEntity> findById(String id) {
         return iProductRepository.findById(id);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<ProductEntity> findBySku(String sku) {
         return iProductRepository.findBySku(sku);
