2f1feaa38253c43498bb29f358d8a02ad3984d4b
jdavidtorres <jesu_ti91@hotmail.com>
2021-04-15 11:10:27 -0500

    Se agrega la relación con Producto y Cliente

--- src/main/java/com/jdti/shoppingcart/models/entities/ShoppingCartEntity.java
+++ src/main/java/com/jdti/shoppingcart/models/entities/ShoppingCartEntity.java
@@ -1,26 +1,43 @@
 package com.jdti.shoppingcart.models.entities;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "shopping_carts")
 @Data
+@NoArgsConstructor
 public class ShoppingCartEntity {
 
-    @NotEmpty
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(name = "id", unique = true, nullable = false, updatable = false)
     private String id;
 
+    @JoinColumn(name = "customer_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private CustomerEntity customer;
+
+    @JoinColumn(name = "product_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ProductEntity product;
+
+    private Integer quantity;
+
+    public ShoppingCartEntity(CustomerEntity customer, ProductEntity product, Integer quantity) {
+        this.customer = customer;
+        this.product = product;
+        this.quantity = quantity;
+    }
 }
